547f475d9c184f226ad77a33c36a73083f140f1a
n3xtchen <echenwen@gmail.com>
2017-03-10 23:50:32 +0800

    键合并

--- src/main/scala/AssociationRules.scala
+++ src/main/scala/AssociationRules.scala
@@ -14,14 +14,14 @@ object AssociationRules {
    * 置信度 miniCon ：同时包含X,Y的文档数/含有X的文档数
    */
 
-  def cartesianProduct[A](list: List[A]) = {
+  def cartesianProduct[A](list: Seq[Seq[A]])(implicit order: Ordering[A]) = {
     for {
       i <- Range(0, list.length) 
       j <- Range(i+1, list.length)
-    } yield (list(i), list(j))
+    } yield (list(i) ++ list(j)).sorted
   }
 
-  def apriori[A](docs: List[List[A]], miniSup: Double) = {
+  def apriori[A](docs: List[List[A]], miniSup: Double)(implicit order: Ordering[A]) = {
 
       var r = List.empty
       val docCnt = docs.length
@@ -29,8 +29,9 @@ object AssociationRules {
         .map(r => (r._1, r._2.length*1.0/docCnt))
 
       val keys = T1.filter(kv => kv._2 >= miniSup).keys
-      println(T1)
-      println(cartesianProduct(keys.toList))
+      val key1 = cartesianProduct(keys.toList.map(List(_)))
+      val key2 = cartesianProduct(key1.toList)
+      println(key2)
   }
 }
 
