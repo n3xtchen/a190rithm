4fe8d5e28fbecfdcafbcb45c6556b95adc9569b1
n3xtchen <echenwen@gmail.com>
2017-03-21 21:08:42 +0800

    关联算法 FP-Tree-reconstructo完成

--- src/test/scala/AssociationRulesTest.scala
+++ src/test/scala/AssociationRulesTest.scala
@@ -29,38 +29,37 @@ class AssociationRulesTest extends FunSpec with Matchers {
     }
 
     it("FP-Tree 构建") {
-      val fpGrowth = FPGrowth(' ')
-      for (item <- D) 
+      val fpGrowth = new FPTree[Char]
+      for (item <- Seq(Seq('b', 'a'), Seq('b'))) 
         fpGrowth.add(item)
-      println(fpGrowth.tree.children('A'))
+      println(fpGrowth.children)
     }
 
     it("FP-Tree 条件模式基") {
-      // val d = fpTree.neighbor('D'.asInstanceOf[A])
-      // println(d)
-      // val dPrefix = FPGrowth.neighborhood(d).map(FPGrowth.prefixPath(_))
-      // for (path <- dPrefix) {
-      //   println(path)
-      // }
-      // var cpb = FPGrowth.conditionPatternBase(dPrefix)
+      def Node(name: Char, cnt: Int) = {
+        var a = new FPNode(name, None, None)
+        a.cnt =cnt
+        a
+      }
 
-      // var dP = cpb.tree 
-      // for (a <- Seq('B', 'C')) {
-      //   dP = dP.children(a.asInstanceOf[A])
-      //   println(dP)
-      // }
-      // dP = cpb.tree 
-      // for (a <- Seq('B')) {
-      //   dP = dP.children(a.asInstanceOf[A])
-      //   println(dP)
-      // }
-      // cpb.neighbor.map(x=> x._1 -> FPGrowth.neighborhood(x._2).map(_.cnt).sum).foreach(println)
+      val cpb = Seq(
+        Seq(Node('a', 5), Node('b', 4), Node('c', 2)),
+        Seq(Node('a', 2), Node('c', 1))
+        )
+      var cpt = FPGrowth.conditionPatternTree(cpb)
+
+      var dP = cpt.children('a')
+      println(dP)
+      for (a <- Seq('b')) {
+        dP = dP.children(a) 
+        println(dP)
+      }
+      cpt.neighbor.map(x=> x._1 -> x._2.neighborhood.map(_.cnt).sum).foreach(println)
     }
 
     it("FPGowth 算法过程") {
-      FPGrowth.fpGrowth(D, miniSup)
+      FPGrowth.fpGrowth(D, miniSup).foreach(println)
     }
-
   }
 }
 
