db4e41b281c4cb880c3aac15dab8d1975f2aa40c
n3xtchen <echenwen@gmail.com>
2023-04-18 12:40:36 +0800

    vimspector 配置

--- tests/version_test.py
+++ tests/version_test.py
@@ -3,7 +3,6 @@
 # Copyright © 2023 n3xtchen <echenwen@gmail.com>
 #
 # Distributed under terms of the GPL-2.0 license.
-
 """
 version.py
 """
@@ -14,4 +13,5 @@ def test_version():
     """
     验证版本
     """
-    assert isinstance(version, str)
+    ver = version()
+    assert isinstance(ver, int)
